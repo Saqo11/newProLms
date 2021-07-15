9bd405dfd3afc8db87768521ddf432741d2b3085
Saqo11 <saqoharutyunyan19@gmail.com>
2021-07-16 00:22:41 +0400

    'dfdfd'

--- src/main/java/com/Eleap/Algorithms/com/Admin/page/UserSectionPage.java
+++ src/main/java/com/Eleap/Algorithms/com/Admin/page/UserSectionPage.java
@@ -1,189 +1,189 @@
-package com.Eleap.Algorithms.com.Admin.page;
-
-import com.Eleap.Algorithms.BasePage;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class UserSectionPage extends BasePage {
-    public UserSectionPage(WebDriver driver) {
-        super(driver);
-    }
-
-    String SITE_URL = ("https://largeaccounttest.eleapdev.com/users/");
-
-    @FindBy(xpath = "//button[text()='Add new user']")
-    private WebElement addNewUserButton;
-
-    @FindBy(xpath = "//div/div[2]/form/div[1]/div[1]/select")
-    private WebElement title;
-
-    @FindBy(css = "[name='first_name']")
-    private WebElement nameField;
-
-    @FindBy(css = "[name='middle_name']")
-    private WebElement middleName;
-
-    @FindBy(css = "[name='last_name']")
-    private WebElement lastName;
-
-    @FindBy(css = "[name='email']")
-    private WebElement emailField;
-
-    @FindBy(xpath = "//button[text()='Add User']")
-    private WebElement addUserButton;
-
-    @FindBy(css = "[placeholder='Search by Name or Email']")
-    private WebElement searchByEmail;
-
-    @FindBy(css = ".d-flex .text-nowrap.clip-text-35")
-    private WebElement displayedUser;
-
-    @FindBy(css = "[name='id[]']")
-    private WebElement checkUser;
-
-    @FindBy(css = ".btn.btn-default")
-    private WebElement applyFilters;
-
-    @FindBy(css = ".select_items_button")
-    private WebElement deleteButton;
-
-    @FindBy(css = ".alert.alert-success")
-    private WebElement success;
-
-    @FindBy(css = "[onclick='showCustomFilterSection(this)']")
-    private WebElement customFieldButton;
-
-    @FindBy(xpath = "//a[text()='ADD FILTER']")
-    private WebElement addFilterButton;
-
-    @FindBy(css = "[title='Deactivate']")
-    private WebElement deactivateButton;
-
-
-    public UserSectionPage goUserSection() {
-        driver.get(SITE_URL);
-        return this;
-    }
-
-    private void clickAddNewUserButton() {
-        addNewUserButton.click();
-    }
-
-    private void chooseTitle() {
-        waitForElementToVisibilityOfElement(driver, title, 15);
-        selectMethod(title, 1);
-    }
-
-    private void fillNameField(CharSequence param) {
-        nameField.sendKeys(param);
-    }
-
-    private void fillMiddleName(CharSequence param) {
-        middleName.sendKeys(param);
-    }
-
-    private void fillLastName(CharSequence param) {
-        lastName.sendKeys(param);
-    }
-
-    private void fillEmailField(CharSequence param) throws InterruptedException {
-        scrollElementDisplayed(driver, lastName);
-        emailField.sendKeys(param);
-    }
-
-    private void clickAddUser() throws InterruptedException {
-        scrollDown(driver);
-        addUserButton.click();
-    }
-
-    private void fillSearchByEmailField(CharSequence param) {
-        searchByEmail.sendKeys(param);
-    }
-
-    private void clickApplyFilters() {
-        applyFilters.click();
-    }
-
-    private void clickToggle() {
-        waitForElementToBeClickable(driver,deactivateButton,10);
-        deactivateButton.click();
-    }
-
-    private void clickCustomFieldButton() {
-        customFieldButton.click();
-    }
-
-    private void clickAddFilterButton() {
-        waitForElementToBeClickable(driver, addFilterButton, 10);
-        addFilterButton.click();
-    }
-
-    private void clickDeleteButton() {
-        checkUser.click();
-        waitForElementToBeClickable(driver, deleteButton, 10);
-        deleteButton.click();
-        alertMethod(driver);
-    }
-
-    private void deleteTheUser() {
-        try {
-            clickDeleteButton();
-
-            boolean UserWasDeleted = success.isDisplayed();
-            assertEquals(true, UserWasDeleted);
-
-        } catch (Exception e) {
-
-
-        }
-    }
-
-
-    public UserSectionPage createNewUser() throws InterruptedException {
-        clickAddNewUserButton();
-        chooseTitle();
-        fillNameField("Robot");
-        fillMiddleName("Auto");
-        fillLastName("User");
-        fillEmailField("rorborbfrf@gcv.ua");
-        clickAddUser();
-
-        return this;
-
-    }
-
-    public UserSectionPage deleteUser() {
-        fillSearchByEmailField("rorborbfrf@gcv.ua");
-        clickApplyFilters();
-        deleteTheUser();
-        return this;
-
-    }
-
-    public UserSectionPage leaveAllFieldEmpty() throws InterruptedException {
-        clickAddNewUserButton();
-        clickAddUser();
-        return this;
-    }
-
-    public UserSectionPage chooseCustomField() {
-        clickCustomFieldButton();
-        clickAddFilterButton();
-        return this;
-
-    }
-
-    public  UserSectionPage deactivateUser(){
-        fillSearchByEmailField("rorborbfrf@gcv.ua");
-        clickApplyFilters();
-        clickToggle();
-        return this;
-
-    }
-
-
-}
+//package com.Eleap.Algorithms.com.Admin.page;
+//
+//import com.Eleap.Algorithms.BasePage;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.support.FindBy;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//public class UserSectionPage extends BasePage {
+//    public UserSectionPage(WebDriver driver) {
+//        super(driver);
+//    }
+//
+//    String SITE_URL = ("https://largeaccounttest.eleapdev.com/users/");
+//
+//    @FindBy(xpath = "//button[text()='Add new user']")
+//    private WebElement addNewUserButton;
+//
+//    @FindBy(xpath = "//div/div[2]/form/div[1]/div[1]/select")
+//    private WebElement title;
+//
+//    @FindBy(css = "[name='first_name']")
+//    private WebElement nameField;
+//
+//    @FindBy(css = "[name='middle_name']")
+//    private WebElement middleName;
+//
+//    @FindBy(css = "[name='last_name']")
+//    private WebElement lastName;
+//
+//    @FindBy(css = "[name='email']")
+//    private WebElement emailField;
+//
+//    @FindBy(xpath = "//button[text()='Add User']")
+//    private WebElement addUserButton;
+//
+//    @FindBy(css = "[placeholder='Search by Name or Email']")
+//    private WebElement searchByEmail;
+//
+//    @FindBy(css = ".d-flex .text-nowrap.clip-text-35")
+//    private WebElement displayedUser;
+//
+//    @FindBy(css = "[name='id[]']")
+//    private WebElement checkUser;
+//
+//    @FindBy(css = ".btn.btn-default")
+//    private WebElement applyFilters;
+//
+//    @FindBy(css = ".select_items_button")
+//    private WebElement deleteButton;
+//
+//    @FindBy(css = ".alert.alert-success")
+//    private WebElement success;
+//
+//    @FindBy(css = "[onclick='showCustomFilterSection(this)']")
+//    private WebElement customFieldButton;
+//
+//    @FindBy(xpath = "//a[text()='ADD FILTER']")
+//    private WebElement addFilterButton;
+//
+//    @FindBy(css = "[title='Deactivate']")
+//    private WebElement deactivateButton;
+//
+//
+//    public UserSectionPage goUserSection() {
+//        driver.get(SITE_URL);
+//        return this;
+//    }
+//
+//    private void clickAddNewUserButton() {
+//        addNewUserButton.click();
+//    }
+//
+//    private void chooseTitle() {
+//        waitForElementToVisibilityOfElement(driver, title, 15);
+//        selectMethod(title, 1);
+//    }
+//
+//    private void fillNameField(CharSequence param) {
+//        nameField.sendKeys(param);
+//    }
+//
+//    private void fillMiddleName(CharSequence param) {
+//        middleName.sendKeys(param);
+//    }
+//
+//    private void fillLastName(CharSequence param) {
+//        lastName.sendKeys(param);
+//    }
+//
+//    private void fillEmailField(CharSequence param) throws InterruptedException {
+//        scrollElementDisplayed(driver, lastName);
+//        emailField.sendKeys(param);
+//    }
+//
+//    private void clickAddUser() throws InterruptedException {
+//        scrollDown(driver);
+//        addUserButton.click();
+//    }
+//
+//    private void fillSearchByEmailField(CharSequence param) {
+//        searchByEmail.sendKeys(param);
+//    }
+//
+//    private void clickApplyFilters() {
+//        applyFilters.click();
+//    }
+//
+//    private void clickToggle() {
+//        waitForElementToBeClickable(driver,deactivateButton,10);
+//        deactivateButton.click();
+//    }
+//
+//    private void clickCustomFieldButton() {
+//        customFieldButton.click();
+//    }
+//
+//    private void clickAddFilterButton() {
+//        waitForElementToBeClickable(driver, addFilterButton, 10);
+//        addFilterButton.click();
+//    }
+//
+//    private void clickDeleteButton() {
+//        checkUser.click();
+//        waitForElementToBeClickable(driver, deleteButton, 10);
+//        deleteButton.click();
+//        alertMethod(driver);
+//    }
+//
+//    private void deleteTheUser() {
+//        try {
+//            clickDeleteButton();
+//
+//            boolean UserWasDeleted = success.isDisplayed();
+//            assertEquals(true, UserWasDeleted);
+//
+//        } catch (Exception e) {
+//
+//
+//        }
+//    }
+//
+//
+//    public UserSectionPage createNewUser() throws InterruptedException {
+//        clickAddNewUserButton();
+//        chooseTitle();
+//        fillNameField("Robot");
+//        fillMiddleName("Auto");
+//        fillLastName("User");
+//        fillEmailField("rorborbfrf@gcv.ua");
+//        clickAddUser();
+//
+//        return this;
+//
+//    }
+//
+//    public UserSectionPage deleteUser() {
+//        fillSearchByEmailField("rorborbfrf@gcv.ua");
+//        clickApplyFilters();
+//        deleteTheUser();
+//        return this;
+//
+//    }
+//
+//    public UserSectionPage leaveAllFieldEmpty() throws InterruptedException {
+//        clickAddNewUserButton();
+//        clickAddUser();
+//        return this;
+//    }
+//
+//    public UserSectionPage chooseCustomField() {
+//        clickCustomFieldButton();
+//        clickAddFilterButton();
+//        return this;
+//
+//    }
+//
+//    public  UserSectionPage deactivateUser(){
+//        fillSearchByEmailField("rorborbfrf@gcv.ua");
+//        clickApplyFilters();
+//        clickToggle();
+//        return this;
+//
+//    }
+//
+//
+//}
--- src/test/java/com/lms/automation/MainTest.java
+++ src/test/java/com/lms/automation/MainTest.java
@@ -1,90 +1,90 @@
-package com.lms.automation;
-
-import com.Eleap.Algorithms.com.Admin.page.*;
-import com.Eleap.Algorithms.user.page.UserDashboardPage;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.TestInstance;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.PageFactory;
-
-import java.util.concurrent.TimeUnit;
-
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-
-public class MainTest {
-    WebDriver driver;
-
-
-    public LoginAdminPage loginAdminPage;
-
-    public CoursesSectionAdminPage coursesSectionAdminPage;
-
-    public DropDownMenuTab dropDownMenuTab;
-
-    public MyAssignedSelfEnrollmentTab myAssignedSelfEnrollmentTab;
-
-    public ManageCategoriesManageEventsTab manageCategoriesManageEventsTab;
-
-    public QuizResultTabs quizResultTabs;
-
-    public ObservationChecklistTab observationChecklistTab;
-
-    public TrainingPathsAdminPage trainingPathsAdminPage;
-
-    public UserDashboardPage userDashboardPage;
-
-    public static OjtSectionPage ojtSectionPage;
-
-    public  static  UserSectionPage userSectionPage;
-
-
-
-
-    @BeforeEach
-    public void start() {
-
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\QA\\IdeaProjects\\New folder (2)\\eleap-autotest\\chromeDriver\\chromedriver.exe");
-
-        driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-
-        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
-
-        loginAdminPage = new LoginAdminPage(driver);
-
-        coursesSectionAdminPage = new CoursesSectionAdminPage(driver);
-
-        dropDownMenuTab = new DropDownMenuTab(driver);
-
-        myAssignedSelfEnrollmentTab = new MyAssignedSelfEnrollmentTab(driver);
-
-        manageCategoriesManageEventsTab = new ManageCategoriesManageEventsTab(driver);
-
-        quizResultTabs = new QuizResultTabs(driver);
-
-        observationChecklistTab = new ObservationChecklistTab(driver);
-
-        trainingPathsAdminPage = new TrainingPathsAdminPage(driver);
-
-        userDashboardPage = new UserDashboardPage(driver);
-
-        ojtSectionPage = new OjtSectionPage(driver);
-
-        userSectionPage = new UserSectionPage(driver);
-
-    }
-
-
-    @AfterEach
-    public void finish() {
-        driver.quit();
-    }
-
-
-}
-
+//package com.lms.automation;
+//
+//import com.Eleap.Algorithms.com.Admin.page.*;
+//import com.Eleap.Algorithms.user.page.UserDashboardPage;
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.TestInstance;
+//import org.openqa.selenium.Dimension;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.support.PageFactory;
+//
+//import java.util.concurrent.TimeUnit;
+//
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//
+//public class MainTest {
+//    WebDriver driver;
+//
+//
+//    public LoginAdminPage loginAdminPage;
+//
+//    public CoursesSectionAdminPage coursesSectionAdminPage;
+//
+//    public DropDownMenuTab dropDownMenuTab;
+//
+//    public MyAssignedSelfEnrollmentTab myAssignedSelfEnrollmentTab;
+//
+//    public ManageCategoriesManageEventsTab manageCategoriesManageEventsTab;
+//
+//    public QuizResultTabs quizResultTabs;
+//
+//    public ObservationChecklistTab observationChecklistTab;
+//
+//    public TrainingPathsAdminPage trainingPathsAdminPage;
+//
+//    public UserDashboardPage userDashboardPage;
+//
+//    public static OjtSectionPage ojtSectionPage;
+//
+//    public  static  UserSectionPage userSectionPage;
+//
+//
+//
+//
+//    @BeforeEach
+//    public void start() {
+//
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\QA\\IdeaProjects\\New folder (2)\\eleap-autotest\\chromeDriver\\chromedriver.exe");
+//
+//        driver = new ChromeDriver();
+//
+//        driver.manage().window().maximize();
+//
+//        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+//
+//        loginAdminPage = new LoginAdminPage(driver);
+//
+//        coursesSectionAdminPage = new CoursesSectionAdminPage(driver);
+//
+//        dropDownMenuTab = new DropDownMenuTab(driver);
+//
+//        myAssignedSelfEnrollmentTab = new MyAssignedSelfEnrollmentTab(driver);
+//
+//        manageCategoriesManageEventsTab = new ManageCategoriesManageEventsTab(driver);
+//
+//        quizResultTabs = new QuizResultTabs(driver);
+//
+//        observationChecklistTab = new ObservationChecklistTab(driver);
+//
+//        trainingPathsAdminPage = new TrainingPathsAdminPage(driver);
+//
+//        userDashboardPage = new UserDashboardPage(driver);
+//
+//        ojtSectionPage = new OjtSectionPage(driver);
+//
+//        userSectionPage = new UserSectionPage(driver);
+//
+//    }
+//
+//
+//    @AfterEach
+//    public void finish() {
+//        driver.quit();
+//    }
+//
+//
+//}
+//
