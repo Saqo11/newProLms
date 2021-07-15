5ce52721c54f2ced71faa555fb3b92c9ad2cedd1
Saqo11 <saqoharutyunyan19@gmail.com>
2021-07-16 00:16:40 +0400

    'dfdfdf'

--- src/test/java/com/lms/automation/LoginAdminTest.java
+++ src/test/java/com/lms/automation/LoginAdminTest.java
@@ -1,8 +1,39 @@
 package com.lms.automation;
 
+import com.Eleap.Algorithms.com.Admin.page.LoginAdminPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LoginAdminTest extends MainTest {
+public class LoginAdminTest  {
+    WebDriver driver;
+    public LoginAdminPage loginAdminPage;
+    @BeforeAll
+    static void setupClass() {
+
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+
+
+
+        driver = new ChromeDriver();
+        loginAdminPage = new LoginAdminPage(driver);
+    }
+
+    @AfterEach
+    void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
     @Test
     public void SingInPositiveTest() {
         loginAdminPage.goTo()
@@ -13,17 +44,5 @@ public class LoginAdminTest extends MainTest {
 
 
     }
-
-    @Test
-    public void SingInNegativeTest() {
-        loginAdminPage.goTo()
-                .fillEmailField("saqo21harustyunyan+19@gmail.com")
-                .fillPassField("7rujds12f")
-                .clickLoginButton()
-                .ifDisplayedAnErrorMessage();
-
-
-    }
-
-
 }
+
